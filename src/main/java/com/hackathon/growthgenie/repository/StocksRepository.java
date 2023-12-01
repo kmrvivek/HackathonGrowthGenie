55fcab2a314c866f83f9f9978d7d9f929461b5ee
tarun madan <32153493+tarunmadan15@users.noreply.github.com>
2023-12-01 20:53:10 +0530

    get investmentByCustomerId

--- src/main/java/com/hackathon/growthgenie/repository/StocksRepository.java
+++ src/main/java/com/hackathon/growthgenie/repository/StocksRepository.java
@@ -2,7 +2,14 @@ package com.hackathon.growthgenie.repository;
 
 import com.hackathon.growthgenie.model.Stocks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface StocksRepository extends JpaRepository<Stocks, Long> {
 //    Page<Stocks> findByInvestmentAccountIDIn(List<String> ids, Pageable pageable);
+   List<Stocks> findByInvestmentAccountID(String investmentAccountID);
+
+
 }
